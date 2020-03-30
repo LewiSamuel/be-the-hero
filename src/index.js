4ab9d0b08a9efc8816e02a8a1bf295de1cecef0a
Lewi Samuel <arroba.lewi@gmail.com>
2020-03-30 14:14:49 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
 const routes = require('./routes')
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
-app.listen(3000)
+app.listen(3000);
