7819b3b9d3d89eaa054d28733fa5009c960e98df
Lewi Samuel <arroba.lewi@gmail.com>
2020-03-30 12:55:44 -0300

    incident

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 const ongController = require('./controllers/ongController');
-
+const incidentController = require('./controllers/incidentController');
 
 routes.get('/', function (req, res) {
     res.send('Hello Wold');
@@ -11,5 +11,8 @@ routes.get('/', function (req, res) {
 routes.post('/ongs', ongController.create);
 routes.get('/ongs', ongController.index);
 
+routes.post('/incident', incidentController.create);
+routes.get('/incident', incidentController.index);
+routes.delete('/incident/:id', incidentController.delete);
 
 module.exports = routes;
